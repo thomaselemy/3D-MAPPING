f95bcb057b81f3c9d682c19dc8a32be1c7beee44
Unknown <thomaselemy@gmail.com>
2018-10-23 16:45:16 -0700

    added the mathcreatemap file

---
+++ mathcreatemap/cmake-build-debug/CMakeFiles/mathcreatemap.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mathcreatemap.dir/main.cpp.o"
+  "CMakeFiles/mathcreatemap.dir/math.cpp.o"
+  "mathcreatemap.pdb"
+  "mathcreatemap"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mathcreatemap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
