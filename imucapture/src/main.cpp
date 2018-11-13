23c704d3f26f7f9433f9fe71c1d35ecbe5939a60
Jaedim <Jaedim@lidar.lxd>
2018-11-13 21:41:57 +0000

    Removed changemode function (unused function)

--- imucapture/src/main.cpp
+++ imucapture/src/main.cpp
@@ -51,20 +51,6 @@
 #include <sys/types.h>
 #include <sys/time.h>
 
-void changemode(int dir)
-{
-    static struct termios oldt, newt;
-
-    if (dir == 1) {
-        tcgetattr( STDIN_FILENO, &oldt);
-        newt = oldt;
-        newt.c_lflag &= ~( ICANON | ECHO );
-        tcsetattr( STDIN_FILENO, TCSANOW, &newt);
-    } else {
-        tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
-	}
-}
-
 inline int kbhit ()
 {
     struct timeval tv;
